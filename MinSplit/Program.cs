3eb60c977ada1e06c0e40c66907509d75161444d
zuka <zuka@DESKTOP-01OQQMQ>
2022-07-13 21:32:17 +0400

    Add project files.

---
+++ MinSplit/Program.cs
@@ -0,0 +1,14 @@
+﻿static int MinSplit(int amount)
+{
+    int[] coins = new int[5] { 50, 20, 10, 5, 1 };
+    int count = 0;
+    foreach (int coin in coins)
+    {
+        count += amount / coin;
+        amount %= coin;
+        if (amount == 0)
+            break;
+    }
+    return count;
+}
+Console.WriteLine(MinSplit(337));
